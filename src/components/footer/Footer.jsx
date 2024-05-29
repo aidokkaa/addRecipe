b0b02aa2d63526b4cf881866d81efa076848765d
Aida <aida.ms0097@gmail.com>
2024-05-29 16:03:41 -0700

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './footer.css'
+export const Footer = () => {
+  return (
+    <div>
+      <div className="footer">
+        <div className="footerDesc">
+        All rights to materials located on this website 
+        are protected in accordance with current legislation. 
+        The site administration may not share the opinions of the authors of articles posted on this site 
+        </div>
+      </div>
+    </div>
+  )
+}
